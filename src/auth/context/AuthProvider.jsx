8906bca7fd1ab8befbbd8f1fbe0fbb2666a6e7aa
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-06 16:55:51 -0400

    [Hero Context]: Logout

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,19 +3,39 @@ import { AuthContext } from "./AuthContext";
 import { authReducer } from "./authReducer";
 import { types } from "../types/types";
 
-const initialState = {
-  logged: false,
+// const initialState = {
+//   logged: false,
+// };
+
+const init = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  return {
+    logged: !!user, // user ? true: false
+    user,
+  };
 };
+
 export const AuthProvider = ({ children }) => {
-  const [authState, dispatch] = useReducer(authReducer, initialState);
+  const [authState, dispatch] = useReducer(authReducer, {}, init);
 
   const login = async (name = "") => {
+    const user = { id: "ABC", name };
+
     const action = {
       type: types.login,
-      payload: {
-        id: "ABC",
-        name: name,
-      },
+      payload: user,
+    };
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    dispatch(action);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    const action = {
+      type: types.logout,
     };
 
     dispatch(action);
@@ -26,6 +46,7 @@ export const AuthProvider = ({ children }) => {
       value={{
         ...authState,
         login,
+        logout,
       }}
     >
       {children}
